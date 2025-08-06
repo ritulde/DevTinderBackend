c66c27d2b5ea9015a8348c853eda5b8aa38dadb1
RITUL DE <ritul-de-bl19-2008@gcect.ac.in>
2025-08-06 10:21:06 +0530

    explore http method

--- src/app.js
+++ src/app.js
@@ -4,9 +4,39 @@ const app=express();//instance of express
 app.listen(3000,()=>{
     console.log("server successfully listen onport 3000")
 });
+//get call
+app.get("/user",(req,res)=>{
+    res.send({firstName: 'Ritul', lastName:'De'})
+})
+
+
+//post call
+app.post("/user",(req,res)=>{
+    //Save Data in DB
+    res.send("Data save successfully")
+})
+
+
+//delete call
+app.delete("/user",(req,res)=>{
+    //Delete user
+    res.send("User deleted successfully")
+})
+
+
+app.patch("/user",(req,res)=>{
+    res.send("User updated successfully Via path")
+})
+app.put("/user",(req,res)=>{
+    res.send("User updated successfully via patch")
+})
+
 app.use("/test",(req,res)=>{
     res.send("test from server");
 })
 app.use("/hello",(req,res)=>{
     res.send("Hello from server");
+})
+app.use("/",(req,res)=>{
+    res.send("Get Call");
 })

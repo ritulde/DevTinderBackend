17b90ca79d206eaaa790f6d7cb84f222d07a8123
RITUL DE <ritul-de-bl19-2008@gcect.ac.in>
2025-08-06 14:16:45 +0530

    Multiple route handler

--- src/app.js
+++ src/app.js
@@ -4,39 +4,55 @@ const app=express();//instance of express
 app.listen(3000,()=>{
     console.log("server successfully listen onport 3000")
 });
-//get call
-app.get("/user",(req,res)=>{
-    res.send({firstName: 'Ritul', lastName:'De'})
-})
+
+app.use("/user",(req,res,next)=>{
+    console.log("1st route handler");
+next();
+},
+[(req,res,next)=>{
+    next();
+res.send("Hi")
+},(req,res)=>{
+    res.send("hello");
+}])
 
 
-//post call
-app.post("/user",(req,res)=>{
-    //Save Data in DB
-    res.send("Data save successfully")
-})
 
 
-//delete call
-app.delete("/user",(req,res)=>{
-    //Delete user
-    res.send("User deleted successfully")
-})
+
+// //get call
+// app.get("/user",(req,res)=>{
+//     res.send({firstName: 'Ritul', lastName:'De'})
+// })
 
 
-app.patch("/user",(req,res)=>{
-    res.send("User updated successfully Via path")
-})
-app.put("/user",(req,res)=>{
-    res.send("User updated successfully via patch")
-})
+// //post call
+// app.post("/user",(req,res)=>{
+//     //Save Data in DB
+//     res.send("Data save successfully")
+// })
 
-app.use("/test",(req,res)=>{
-    res.send("test from server");
-})
-app.use("/hello",(req,res)=>{
-    res.send("Hello from server");
-})
-app.use("/",(req,res)=>{
-    res.send("Get Call");
-})
+
+// //delete call
+// app.delete("/user",(req,res)=>{
+//     //Delete user
+//     res.send("User deleted successfully")
+// })
+
+
+// app.patch("/user",(req,res)=>{
+//     res.send("User updated successfully Via path")
+// })
+// app.put("/user",(req,res)=>{
+//     res.send("User updated successfully via patch")
+// })
+
+// app.use("/test",(req,res)=>{
+//     res.send("test from server");
+// })
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello from server");
+// })
+// app.use("/",(req,res)=>{
+//     res.send("Get Call");
+// })

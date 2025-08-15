2e7fe86a72c6f4b40f01136f30a297523b3c98cc
RITUL DE <ritul-de-bl19-2008@gcect.ac.in>
2025-08-15 22:30:59 +0530

    send correct message

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
     const cookies = req.cookies;
     const { token } = cookies;
     if (!token) {
-      throw new Error("Invalid token");
+      res.status(401).send("Please login");
     }
     const decodedMessage = await jwt.verify(token, "DevTinder@790");
     const { _id } = decodedMessage;
